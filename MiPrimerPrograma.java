9b77e7293594d5d9f1b323cec8e5b8bb12f7cbef
Enzo Pérez <enzo.pereztm@gmail.com>
2021-01-06 23:29:25 -0300

    Tarea mi primer programa

---
+++ MiPrimerPrograma.java
@@ -0,0 +1,7 @@
+public class MiPrimerPrograma{
+    public static void main(String[] args){
+        System.out.println("Mi nombre es Enzo");
+        System.out.println("Tengo 38 años de edad");
+        System.out.println("Mi ciudad es Temuco");
+    }
+}
