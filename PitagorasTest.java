64d50886a10ad8fa32a2716dca642b409993ce7e
Enzo Pérez <enzo.pereztm@gmail.com>
2021-01-08 11:45:13 -0300

    Tarea Pitagoras

---
+++ PitagorasTest.java
@@ -0,0 +1,11 @@
+package ejercicioPitagoras;
+
+public class PitagorasTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pitagoras pitagoras = new Pitagoras();
+		System.out.println(pitagoras.calcularHipotenusa(4,4));
+	}
+
+}
