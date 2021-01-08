ed1eeab565319d0ad100f1af3f212abf298a3797
Enzo Pérez <enzo.pereztm@gmail.com>
2021-01-08 20:09:00 -0300

    prueba

---
+++ Enzo/src/Enzo.java
@@ -0,0 +1,15 @@
+
+public class Enzo {
+	
+
+		public static Double main(String[] args) {
+			    double calcularHipotenusa(int catetoA, int catetoB)
+				double hipotenusa;
+				hipotenusa = Math.sqrt((catetoA*catetoA)+(catetoB*catetoB));
+				return hipotenusa;
+				System.out.println(hipotenusa);
+			
+	}
+
+
+}
