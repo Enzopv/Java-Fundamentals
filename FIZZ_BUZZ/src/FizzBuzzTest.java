595829d42b6ff71f541f67a762079dbe5d63e373
Enzo Pérez <enzo.pereztm@gmail.com>
2021-01-12 10:44:37 -0300

    Tarea FizzBuzz

---
+++ FIZZ_BUZZ/src/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+
+public class FizzBuzzTest {
+
+	public static void main(String[] args) {
+		FizzBuzz T=new FizzBuzz();
+		System.out.println(T.fizzBuzz(3));
+		System.out.println(T.fizzBuzz(5));
+		System.out.println(T.fizzBuzz(15));
+		System.out.println(T.fizzBuzz(2));
+	}
+	
+}
